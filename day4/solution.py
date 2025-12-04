71b609236add74660da7d46d0ef10f61c64ea01c
PereAPrats <ppratsv03@gmail.com>
2025-12-04 12:33:01 +0100

    Started day 4 challenge

---
+++ day4/solution.py
@@ -0,0 +1,13 @@
+"""
+This is the fourth challenge of Advent Code 2025.
+"""
+"""
+Part 1:
+You need to help the Elves optimize the work of the forklifts in the 
+printing department so they can free up time to break through a wall 
+and reach the cafeteria. The department has a large grid representing 
+the positions of paper rolls (@). A forklift can only access a roll 
+if there are fewer than four rolls in the eight adjacent positions 
+(orthogonal and diagonal). Your task is to analyze the grid and 
+determine how many rolls of paper are accessible under this rule.
+"""
